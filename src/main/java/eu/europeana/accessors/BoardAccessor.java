6e586f981c627ffc67cf7f797df2379bcf082b5d
stzanakis <simon.tzanakis@europeana.eu>
2016-06-22 13:47:06 +0200

    Update javadoc

--- src/main/java/eu/europeana/accessors/BoardAccessor.java
+++ src/main/java/eu/europeana/accessors/BoardAccessor.java
@@ -14,8 +14,32 @@ import java.net.URL;
 public interface BoardAccessor {
 
     String getBoardInformation(String user, String board);
+
+    /** Retrieves Pins from a specific User and Board. Returns only a set of pins.
+     * @param user
+     * @param board
+     * @return {@link PinsData}
+     * @throws DoesNotExistException
+     * @throws BadRequest
+     */
     PinsData getPinsFromBoard(String user, String board) throws DoesNotExistException, BadRequest;
+
+    /** Retrieves Pins from a provided URL. Used mostly for collecting all pins with multiple calls.
+     * @param url
+     * @return
+     * @throws DoesNotExistException
+     * @throws BadRequest
+     */
     PinsData getPinsFromBoard(URL url) throws DoesNotExistException, BadRequest;
+
+    /** Retrieves all Pins from a specific User and Board. Makes multiple calls using pagination.
+     * @param user
+     * @param board
+     * @return
+     * @throws DoesNotExistException
+     * @throws BadRequest
+     * @throws MalformedURLException
+     */
     PinsData getAllPinsFromBoard(String user, String board) throws DoesNotExistException, BadRequest, MalformedURLException;
 
     /**
